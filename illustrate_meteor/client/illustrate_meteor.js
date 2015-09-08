eda3338279fdd5b68abfeb57f6db1381f85df60b
Arjun Bali <arjun.bali96@gmail.com>
2015-09-07 23:09:34 -0400

    Abstraction of renderCavnas into canvas_methods

--- illustrate_meteor/client/illustrate_meteor.js
+++ illustrate_meteor/client/illustrate_meteor.js
@@ -1,34 +1,3 @@
-function renderCanvas(path_points) {
-  paper.setup('canvas');
-  var tool = new paper.Tool();
-  var line_id;
-  var path;
-
-  tool.onMouseDown = function(event) {
-    path = new paper.Path();
-    path.strokeColor = 'black';
-    path.add(event.point);
-    line_id = Lines.insert({createdAt: new Date()});
-    Lines.update(
-      {_id: line_id},
-      {$push : {points: [event.point.x, event.point.y]}}
-    );
-    console.log(Lines.find(line_id));
-  };
-
-  tool.onMouseDrag = function(event) {
-    path.add(event.point);
-    Lines.update({_id: line_id}, {$push : {points: [event.point.x, event.point.y]}});
-  };
-
-  tool.onMouseUp = function(event) {
-    path.smooth();
-    path.simplify();
-  };
-}
-
-if (Meteor.isClient) {
-  Template.canvas.onRendered(function (){
-    renderCanvas();
-  });
-}
+Template.canvas.onRendered(function (){
+	Meteor.canvasMethods.renderCanvas();
+});
