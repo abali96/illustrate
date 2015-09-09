708988821fa8ec52e96da54a9e876ff88a7105f9
Arjun Bali <arjun.bali96@gmail.com>
2015-09-09 19:38:01 -0400

    Framework for command line action mapping

--- illustrate_meteor/both/collections/log.js
+++ illustrate_meteor/both/collections/log.js
@@ -1,1 +1,5 @@
 Logs = new Mongo.Collection('logs');
+
+Logs.after.insert(function () {
+	$('#command_line_history').scrollTop($('#command_line_history')[0].scrollHeight); // scroll to the bottom of the log
+});
--- illustrate_meteor/client/lib/command_line/command_line_methods.js
+++ illustrate_meteor/client/lib/command_line/command_line_methods.js
@@ -3,3 +3,36 @@ Meteor.logger = {
 		Logs.insert({text: data, type: type, broadcast: broadcast});
 	},
 };
+
+Meteor.commandLineMethods = {
+	executeCommand : function(command) { // parse the command to do the desired action
+		action_map = {
+			"line": Meteor.commandLineMethods.setLineTool,
+			"scribble": Meteor.commandLineMethods.setScribbleTool,
+			"colour": Meteor.commandLineMethods.setColour,
+			"width": Meteor.commandLineMethods.setStrokeWidth,
+			"dash": Meteor.commandLineMethods.setStrokeWidth,
+			"end": Meteor.commandLineMethods.endLine,
+			"debug": Meteor.commandLineMethods.debug,
+		};
+		possible_actions = [];
+		command = command.toLowerCase();
+		for (var action in action_map) { // Find all those actions that the user's command could relate to
+			if (action_map.hasOwnProperty(action) && action.startsWith(command)) {
+				possible_actions.push(action);
+			}
+		}
+		console.log(possible_actions);
+		if (possible_actions.length === 0) {
+			Logs.insert({text: command_line_value, type: "Unknown Command"});
+		} else if (possible_actions.length == 1) {
+			Logs.insert({text: command_line_value, type: "Command", resultant_command: possible_actions[0]});
+			action_map[possible_actions[0]]();
+		} else {
+			Logs.insert({text: possible_actions.join(", "), type: "Possible commands with given input: '" + command_line_value + "'"});
+		}
+	},
+	debug : function(command) {
+		alert("yes?");
+	},
+};
